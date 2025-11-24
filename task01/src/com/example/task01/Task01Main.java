57b25722ccd985b666d72312eef6a5cae8727532
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-11-24 19:56:31 +0500

    Update Task01Main.java

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -23,7 +23,7 @@ public class Task01Main {
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
 
-        return null; // your implementation here
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
 
     }
 }
